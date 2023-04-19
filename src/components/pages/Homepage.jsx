9256e9a1c24ae0fec7e9dc07733b8100c01f772d
Minh Cuong <tranminhcuong3101@gmail.com>
2023-04-19 16:23:47 +0700

    design boilerplate

---
+++ src/components/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import { Footer, Navbar } from "../common";
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <main>Hello Địt mẹ m</main>
+      <Footer></Footer>
+    </>
+  );
+}
