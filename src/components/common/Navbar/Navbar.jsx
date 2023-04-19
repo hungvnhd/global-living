9505eb7ee2f80dd94672e93567a8bdd9273aa02f
Minh Cuong <tranminhcuong3101@gmail.com>
2023-04-19 19:23:40 +0700

    update navbar

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -1,3 +1,24 @@
+import Link from "next/link";
+
 export default function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="Navbar">
+      <div className="additional-info">
+        <div className="phone">0931 626 909</div>
+        <div className="icon">fb</div>
+        <div className="icon">zl</div>
+        <div className="icon">ytb</div>
+      </div>
+      <div className="logo">
+        <img src="/logo.png" alt="global-living" />
+      </div>
+      <div className="main-nav">
+        <Link href="/">Về Global Living</Link>
+        <Link href="/">Sản phẩm đầu tư</Link>
+        <Link href="/">Tin tức</Link>
+        <Link href="/">Tài liệu bán hàng</Link>
+        <Link href="/">Đăng nhập Đại lý/ CTV</Link>
+      </div>
+    </nav>
+  );
 }
