14f0bde0fa3a9e33a732a7247a1e830fa7056fd5
Minh Cuong <tranminhcuong3101@gmail.com>
2023-04-21 13:43:06 +0700

    update navbar, header

--- src/components/common/Navbar/Navbar.jsx
+++ src/components/common/Navbar/Navbar.jsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="Navbar">
-      <div className="additional-info">
+      <div className="nav-row additional-info">
         <div className="phone">0931 626 909</div>
         <div className="icon">fb</div>
         <div className="icon">zl</div>
         <div className="icon">ytb</div>
       </div>
-      <div className="logo">
+      <div className="nav-row logo">
         <img src="/logo.png" alt="global-living" />
       </div>
-      <div className="main-nav">
+      <div className="nav-row main-nav">
         <Link href="/">Về Global Living</Link>
         <Link href="/">Sản phẩm đầu tư</Link>
         <Link href="/">Tin tức</Link>
