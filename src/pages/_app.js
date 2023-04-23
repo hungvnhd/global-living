0851c1bf8615bd82e819b802020165e4d2019bc7
hungvnhd <hungacoustichd@gmail.com>
2023-04-23 13:02:56 +0700

    recheck

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
 import "@/styles/Homepage.css";
-<<<<<<< HEAD
-import { useEffect } from "react";
-
-function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
-
-=======
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect } from "react";
 
@@ -18,8 +9,7 @@ export default function App({ Component, pageProps }) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
->>>>>>> feature/signup
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+
